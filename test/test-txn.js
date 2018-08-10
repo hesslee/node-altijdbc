e7deee87d3c2e93d40b3a48ca1a187f69465d1d0
hessalti <hess.lee@altibase.com>
2018-08-10 12:22:42 +0900

    modify altibase portno to 20300

--- test/test-txn.js
+++ test/test-txn.js
@@ -9,7 +9,7 @@ if (!jinst.isJvmCreated()) {
 }
 
 var config = {
-  url: 'jdbc:Altibase://testserver:20999/mydb',
+  url: 'jdbc:Altibase://testserver:20300/mydb',
   user: 'sys',
   password: 'manager'
 };
